13feb2db7ad8c52cbb409a049609e6d9eb3c216c
mak-1997 <107460834+mak-1997@users.noreply.github.com>
2023-03-27 11:11:03 +0530

    origin updated to all

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
-const corsOptions = {
-  origin: [`https://frontend-ochre-beta.vercel.app/`, "https://example.com"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: [`https://frontend-ochre-beta.vercel.app/`, "https://example.com"],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true,
+// };
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors({origin: "*"}));
 
 app.use("/users", userRouter);
 app.use(auth);
