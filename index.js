b5b399e561399309d29e8ff1af0cc6ae35b12db7
mak-1997 <107460834+mak-1997@users.noreply.github.com>
2023-03-27 11:05:05 +0530

    cors updated

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const express = require("express");
-const {connection} = require("./db");
-const {userRouter} = require("./routes/user.route");
-const {todosRouter} = require("./routes/todos.route");
-const {auth} = require("./middleware/auth.middleware");
+const { connection } = require("./db");
+const { userRouter } = require("./routes/user.route");
+const { todosRouter } = require("./routes/todos.route");
+const { auth } = require("./middleware/auth.middleware");
 require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
+const corsOptions = {
+  origin: [`${process.env.mongodbURL}`, "https://example.com"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/users", userRouter);
 app.use(auth);
-app.use("/todos",todosRouter);
+app.use("/todos", todosRouter);
 
-
-app.listen(process.env.port, async()=>{
-    try {
-        await connection;
-        console.log("Connected to Database");
-    } catch (error) {
-        console.log("Cannot connect to the database");
-        console.log(error);
-    }
-    console.log(`Server is running on port ${process.env.port}`);
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to Database");
+  } catch (error) {
+    console.log("Cannot connect to the database");
+    console.log(error);
+  }
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
